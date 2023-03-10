591d394f70c3d58b975afdfca6e0d776ae06c156
Eken Chan <maskedeken@yahoo.com>
2023-03-10 13:47:33 +0800

    fix trojan header not sent issue

--- buildScript/lib/core/clone.sh
+++ buildScript/lib/core/clone.sh
@@ -3,6 +3,6 @@ COMMIT=$(cat libcore/core_commit.txt)
 cd ..
 [ -d v2ray-core ] && exit 0
 rm -rf v2ray-core
-git clone --no-checkout https://github.com/MatsuriDayo/v2ray-core.git
+git clone --no-checkout https://github.com/maskedeken/v2ray-core.git
 cd v2ray-core
 git checkout $COMMIT
